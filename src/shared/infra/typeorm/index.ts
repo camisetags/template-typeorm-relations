8b5de6a7c10ea415931ba7c57069c6ccae3abf19
Gabriel Seixas <salesseixasgabriel@gmail.com>
2020-11-02 02:20:19 -0300

    Changed configs

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'd84jvjqu6e68rl'
+          ? 'gostack_desafio09_tests'
           : defaultOptions.database,
     }),
   );
